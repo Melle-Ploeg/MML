08b6a51c3a9d5679027ab7b7b14b39a61e9c8135
s2398443 <w.h.stribos@student.utwente.nl>
2026-03-31 17:11:44 +0200

    Implemented label generation for stress dataset

--- data_aligner_9000.py
+++ data_aligner_9000.py
@@ -9,24 +9,56 @@ def align_features(sample_dict:dict, time_dict:dict):
     features = ['EDA', 'BVP', 'TEMP', 'ACC']  # Features to align, without HR
     time_array = time_dict['HR']
     length = len(time_array)    # Scale everything to the length of the HR array
-    result = np.zeros((length, len(features) + 2))      # Create empty feature matrix, with one column for each feature
+    result = np.zeros((length, len(features) + 3))      # Create empty feature matrix, with one column for each feature
     result[:,0:1] = sample_dict['HR']
     sample_dict['ACCB'] = (abs(sample_dict['ACC'][:,0]) + abs(sample_dict['ACC'][:,1]) + abs(sample_dict['ACC'][:,2])) / 3
     time_dict['ACCB'] = time_dict['ACC']
     # Main loop creating the feature matrix. Yes the code is ugly.
     start = time.time()
+    label = 2
+    print(sample_dict["tags"])
     for i in range(length):
-        result[i][1] = find_nearest_point(time_array[i], np.ravel(sample_dict['EDA']), np.ravel(time_dict['EDA']))
-        result[i][2] = find_nearest_point(time_array[i], np.ravel(sample_dict['BVP']), np.ravel(time_dict['BVP']))
-        result[i][3] = find_nearest_point(time_array[i], np.ravel(sample_dict['TEMP']), np.ravel(time_dict['TEMP']))
+        result[i][1] = find_avg(time_array[i], np.ravel(sample_dict['EDA']), np.ravel(time_dict['EDA']), 4)
+        result[i][2] = find_std(time_array[i], np.ravel(sample_dict['EDA']), np.ravel(time_dict['EDA']), 4)
+        result[i][3] = find_nearest_point(time_array[i], np.ravel(sample_dict['BVP']), np.ravel(time_dict['BVP']))
+        result[i][4] = find_avg(time_array[i], np.ravel(sample_dict['TEMP']), np.ravel(time_dict['TEMP']), 4)
 
-        result[i][4] = find_avg(time_array[i], np.ravel(sample_dict['ACCB']), np.ravel(time_dict['ACCB']), 64)
-        result[i][5] = find_std(time_array[i], np.ravel(sample_dict['ACCB']), np.ravel(time_dict['ACCB']), 64)
+        result[i][5] = find_avg(time_array[i], np.ravel(sample_dict['ACCB']), np.ravel(time_dict['ACCB']), 64)
+        result[i][6] = find_std(time_array[i], np.ravel(sample_dict['ACCB']), np.ravel(time_dict['ACCB']), 64)
+        
 
-    print(time.time() - start)
-    print(result[:,4])
-    print(result[:,4].shape)
-    print(result[:,5])
+    # print(time.time() - start)
+    # print(result[:,4])
+    # print(result[:,4].shape)
+    # print(result[:,5])
+    return result
+
+def stress_F_Labels(sample_dict:dict, time_dict:dict):
+    length = len(time_dict["HR"])
+    result = np.zeros(length)
+    set_number = 0
+    next_tag = 2
+    #label = 0
+    sample_dict["tags"] += [0]
+    for i in range(length):
+        if i == sample_dict["tags"][next_tag]:
+            set_number = (next_tag) * ((next_tag+1) % 2) #Multiplied by 0 if inbetween two sets
+            next_tag += 1
+        result[i] = int(set_number/2)
+    return result
+
+def stress_S_Labels(sample_dict:dict, time_dict:dict):
+    length = len(time_dict["HR"])
+    result = np.zeros(length)
+    set_number = 0
+    next_tag = 3
+    #label = 0
+    sample_dict["tags"][13] = 0
+    for i in range(length):
+        if i == sample_dict["tags"][next_tag]:
+            set_number = (next_tag-2) * ((next_tag) % 2)
+            next_tag += 1
+        result[i] = int((set_number+1)/2)#int(set_number/2)
     return result
 
 # Find the value of sample data b that is nearest to the time of sample a
@@ -63,7 +95,11 @@ def get_features():
 
 signal_data, time_data, fs_dict, participants = get_features()
 fs_dict = fs_dict['AEROBIC']['f01'] # Sampling frequencies are the same for every experiment so we simplify the dict
-features = align_features(signal_data['STRESS']['f01'], time_data['STRESS']['f01'])
+features = align_features(signal_data['STRESS']['S01'], time_data['STRESS']['S01'])
+labels = stress_S_Labels(signal_data['STRESS']['S01'], time_data['STRESS']['S01'])
+with np.printoptions(edgeitems=4000):
+    print(labels)
+
 # print(features.shape)
 # print(features)
 
