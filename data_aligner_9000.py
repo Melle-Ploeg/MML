bb83f7b8387096e50c5224c827d6276d4cc633cd
Melle-Ploeg <82025734+Melle-Ploeg@users.noreply.github.com>
2026-03-31 17:21:05 +0200

    Labelling for aerobic and anaerobix

--- data_aligner_9000.py
+++ data_aligner_9000.py
@@ -24,9 +24,9 @@ def align_features(sample_dict:dict, time_dict:dict):
         result[i][5] = find_std(time_array[i], np.ravel(sample_dict['ACCB']), np.ravel(time_dict['ACCB']), 64)
 
     print(time.time() - start)
-    print(result[:,4])
-    print(result[:,4].shape)
-    print(result[:,5])
+    # print(result[:,4])
+    # print(result[:,4].shape)
+    # print(result[:,5])
     return result
 
 # Find the value of sample data b that is nearest to the time of sample a
@@ -61,10 +61,60 @@ def get_features():
 
     return signal_data, time_data, fs_dict, participants
 
+
+def label_aerobic(aligned_data, sample_dict):
+    result = {}
+    bad_keys = ['S03', 'S07', 'S11_a', 'S11_b', 'S12']
+    for subject in aligned_data.keys():
+        if not subject in bad_keys:
+            labels = np.zeros(len(aligned_data[subject][:,0]))
+            tags = sample_dict[subject]['tags']
+            labels[tags[1]:tags[-2]] = 1
+            result[subject] = labels
+    return result
+
+
+def label_anaerobic(aligned_data, sample_dict):
+    result = {}
+    bad_keys = ['S06', 'S16_a', 'S16_b']
+    for subject in aligned_data.keys():
+        if not subject in bad_keys:
+            print(subject)
+            labels = np.zeros(len(aligned_data[subject][:, 0]))
+            tags = sample_dict[subject]['tags']
+            if 'S' in subject:
+                # Three tests
+                for i in range(0, 6, 2):
+                    start, end = tags[i], tags[i + 1]
+                    labels[start:end] = 1
+                    result[subject] = labels
+            else:
+                # Four tests
+                for i in range(2, 10, 2):
+                    start, end = tags[i], tags[i + 1]
+                    labels[start:end] = 1
+                    result[subject] = labels
+    return result
+
+def align_all(sample_dict, time_dict):
+    result = {}
+    for subject in sample_dict.keys():
+        result[subject] = align_features(sample_dict[subject], time_dict[subject])
+    return result
+
+
 signal_data, time_data, fs_dict, participants = get_features()
-fs_dict = fs_dict['AEROBIC']['f01'] # Sampling frequencies are the same for every experiment so we simplify the dict
-features = align_features(signal_data['STRESS']['f01'], time_data['STRESS']['f01'])
+print(signal_data['ANAEROBIC']['f01']['tags'])
+fs_dict = fs_dict['ANAEROBIC']['f01'] # Sampling frequencies are the same for every experiment so we simplify the dict
+features = align_features(signal_data['ANAEROBIC']['f01'], time_data['ANAEROBIC']['f01'])
 # print(features.shape)
 # print(features)
+np.set_printoptions(threshold=np.inf)
+test_dict = {'f01':features}
+print(label_anaerobic(test_dict, signal_data['ANAEROBIC'])['f01'])
+
+for s in signal_data['ANAEROBIC'].keys():
+    print(s)
+    print(len(signal_data['ANAEROBIC'][s]['tags']))
 
 
