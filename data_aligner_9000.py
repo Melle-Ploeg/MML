469e921c859b26ea54344dbdbb1f41f5a27e3561
Melle-Ploeg <82025734+Melle-Ploeg@users.noreply.github.com>
2026-03-31 14:48:45 +0200

    Change data alignment to turn acceleration in 3 axes to average acceleration and standard deviation acceleration for each second

--- data_aligner_9000.py
+++ data_aligner_9000.py
@@ -9,24 +9,38 @@ def align_features(sample_dict:dict, time_dict:dict):
     features = ['EDA', 'BVP', 'TEMP', 'ACC']  # Features to align, without HR
     time_array = time_dict['HR']
     length = len(time_array)    # Scale everything to the length of the HR array
-    result = np.zeros((length, len(features) + 3))      # Create empty feature matrix, with one column for each feature
+    result = np.zeros((length, len(features) + 2))      # Create empty feature matrix, with one column for each feature
     result[:,0:1] = sample_dict['HR']
+    sample_dict['ACCB'] = (abs(sample_dict['ACC'][:,0]) + abs(sample_dict['ACC'][:,1]) + abs(sample_dict['ACC'][:,2])) / 3
+    time_dict['ACCB'] = time_dict['ACC']
     # Main loop creating the feature matrix. Yes the code is ugly.
     start = time.time()
     for i in range(length):
         result[i][1] = find_nearest_point(time_array[i], np.ravel(sample_dict['EDA']), np.ravel(time_dict['EDA']))
         result[i][2] = find_nearest_point(time_array[i], np.ravel(sample_dict['BVP']), np.ravel(time_dict['BVP']))
         result[i][3] = find_nearest_point(time_array[i], np.ravel(sample_dict['TEMP']), np.ravel(time_dict['TEMP']))
-        result[i][4] = find_nearest_point(time_array[i], sample_dict['ACC'][:,0], time_dict['ACC'])
-        result[i][5] = find_nearest_point(time_array[i], sample_dict['ACC'][:,1], time_dict['ACC'])
-        result[i][6] = find_nearest_point(time_array[i], sample_dict['ACC'][:,2], time_dict['ACC'])
+
+        result[i][4] = find_avg(time_array[i], np.ravel(sample_dict['ACCB']), np.ravel(time_dict['ACCB']), 64)
+        result[i][5] = find_std(time_array[i], np.ravel(sample_dict['ACCB']), np.ravel(time_dict['ACCB']), 64)
+
     print(time.time() - start)
+    print(result[:,4])
+    print(result[:,4].shape)
+    print(result[:,5])
     return result
 
 # Find the value of sample data b that is nearest to the time of sample a
 def find_nearest_point(a_time, b_sample, b_times):
     idx = (np.abs(b_times - a_time)).argmin()
     return b_sample[idx]
+
+def find_avg(a_time, b_sample, b_times, b_fs):
+    idx = (np.abs(b_times - a_time)).argmin()
+    return np.average(b_sample[idx:idx+b_fs])
+
+def find_std(a_time, b_sample, b_times, b_fs):
+    idx = (np.abs(b_times - a_time)).argmin()
+    return np.std(b_sample[idx:idx+b_fs])
 
 def get_features():
     dataset_path='data/Wearable_Dataset/'#replace the folder path
@@ -50,7 +64,7 @@ def get_features():
 signal_data, time_data, fs_dict, participants = get_features()
 fs_dict = fs_dict['AEROBIC']['f01'] # Sampling frequencies are the same for every experiment so we simplify the dict
 features = align_features(signal_data['STRESS']['f01'], time_data['STRESS']['f01'])
-print(features.shape)
-print(features)
+# print(features.shape)
+# print(features)
 
 
