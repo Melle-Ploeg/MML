5011fb52a3a032c945a4ac66624fa4b773fc95df
Melle-Ploeg <82025734+Melle-Ploeg@users.noreply.github.com>
2026-04-09 10:50:18 +0200

    softMax added

--- train.py
+++ train.py
@@ -13,10 +13,15 @@ class ManyToManyLSTM(nn.Module):
         super(ManyToManyLSTM, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
+        self.softAct = nn.Softmax(dim=2)
 
     def forward(self, x):
         out, _ = self.lstm(x)
         out = self.fc(out)
+        # print("Hi!")
+        # print(out[0,0,:])
+        out = self.softAct(out)
+        # print(out[0,0,:])
         return out
 
 # Take a matrix of all the labeled input data
@@ -25,17 +30,19 @@ def train(X_train, y_train, X_test, y_test):
     y_train = torch.tensor(y_train, dtype=torch.float32)
     # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     # X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2)
+    X_test = torch.tensor(X_test, dtype=torch.float32)
+    y_test = torch.tensor(y_test, dtype=torch.float32)
 
     loader = data.DataLoader(data.TensorDataset(X_train, y_train), shuffle=True, batch_size=8)
     # Define model parameters
     input_size = 7
 
     hidden_size = 32
-    num_layers = 1
-    output_size = 1
+    num_layers = 2
+    output_size = 4
     model = ManyToManyLSTM(input_size, hidden_size, num_layers, output_size)
 
-    criterion = nn.MSELoss()
+    criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     # Train the model
@@ -44,7 +51,7 @@ def train(X_train, y_train, X_test, y_test):
         model.train()
         for X_batch, y_batch in loader:
             y_pred = model(X_batch)
-            y_pred = torch.squeeze(y_pred, 2)
+            # y_pred = torch.squeeze(y_pred, 2)
             loss = criterion(y_pred, y_batch)
             optimizer.zero_grad()
             loss.backward()
@@ -55,12 +62,15 @@ def train(X_train, y_train, X_test, y_test):
         model.eval()
         with torch.no_grad():
             y_pred = model(X_train)
-            y_pred = torch.squeeze(y_pred, 2)
-            train_rmse = np.sqrt(criterion(y_pred, y_train))
+            print(y_pred.shape, y_train.shape)
+            print(y_pred[10, 100, :])
+            print(y_train[10, 100, :])
+            # y_pred = torch.squeeze(y_pred, 2)
+            train_loss = np.sqrt(criterion(y_pred, y_train))
             y_pred = model(X_test)
-            y_pred = torch.squeeze(y_pred, 2)
-            test_rmse = np.sqrt(criterion(y_pred, y_test))
-        print("Epoch %d: train RMSE %.4f, test RMSE %.4f" % (epoch, train_rmse, test_rmse))
+            # y_pred = torch.squeeze(y_pred, 2)
+            test_loss = np.sqrt(criterion(y_pred, y_test))
+        print("Epoch %d: train :̶.̶|̶ ̶:̶;̶ %.4f, test :̶.̶|̶ ̶:̶;̶ %.4f" % (epoch, train_loss, test_loss))
 
 
 
