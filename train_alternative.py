1cac7256284777412950947593438bdc382c4be2
s2398443 <w.h.stribos@student.utwente.nl>
2026-04-11 15:43:53 +0200

    Alternative train_alternative

--- train_alternative.py
+++ train_alternative.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 import numpy as np
 from torch.autograd import Variable
 import torch.utils.data as data
+from ignite import metrics
 
 
 class ManyToManyLSTM(nn.Module):
@@ -14,13 +15,15 @@ class ManyToManyLSTM(nn.Module):
         self.hidden_dim = hidden_dim
         self.layer_dim = num_layers
         super(ManyToManyLSTM, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_dim, num_layers, batch_first=True, device=device)
-        self.fc = nn.Linear(hidden_dim, output_size)
+        self.lstm = nn.LSTM(input_size, hidden_dim, num_layers, batch_first=True, device=device, dropout=0.1)
+        self.lstm2 = nn.LSTM(hidden_dim, int(hidden_dim/2), num_layers, batch_first=True, device=device, dropout=0.1)
+        self.fc = nn.Linear(int(hidden_dim/2), output_size)
         self.softAct = nn.Softmax(dim=1)
 
 
     def forward(self, x):
         out, _ = self.lstm(x)
+        out, _ = self.lstm2(out)
         out = self.fc(out)
         out = self.softAct(out[:,-1,:])
         return out
@@ -53,18 +56,22 @@ def train(X_train, y_train, X_test, y_test, device, batch_size=8):
     input_size = 5
 
     hidden_size = 256
-    num_layers = 3
+    num_layers = 1
     output_size = 3
     model = ManyToManyLSTM(input_size, hidden_size, num_layers, output_size, device)
     model.to(device)
 
     criterion = nn.CrossEntropyLoss()
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.90, weight_decay=1e-5)#, nesterov=True)
 
     # h0, c0 = None, None
     # Train the model
+    topAcc = 0
     n_epochs = 2000
+    accuracies = metrics.Accuracy()
+    recall = metrics.Recall()
+    confusion_matrix = metrics.ConfusionMatrix(output_size)
     for epoch in range(n_epochs):
         model.train()
         for X_batch, y_batch in loader:
@@ -78,10 +85,11 @@ def train(X_train, y_train, X_test, y_test, device, batch_size=8):
             optimizer.step()
         # h0, c0 = h0.detach(), c0.detach()
         # Validation
-        if epoch % 20 != 0:
+        if epoch % 10 != 0:
             continue
         model.eval()
         with torch.no_grad():
+            accuracies.reset()
             y_pred = model(X_train)
             # print(y_pred.shape, y_train.shape)
             # print(y_pred[10, 100, :])
@@ -92,20 +100,47 @@ def train(X_train, y_train, X_test, y_test, device, batch_size=8):
             print(y_pred.shape)
             # y_pred = torch.squeeze(y_pred, 2)
             test_loss = criterion(y_pred, y_test)
+
             for i in range(y_pred.shape[0]):
                 print(y_test[i])
                 print(y_pred[i])
-            print(acc(y_pred, y_test))
+            accuracies.update((y_pred, y_test))
+            current_acc = accuracies.compute() #acc(y_pred, y_test)
+
+            print("Accuracy:", current_acc)
+            
+            recall.reset()
+            recall.update((y_pred, y_test))
+            current_recall = recall.compute()
+            print("recall:", current_recall)
+
+            if(current_acc > topAcc):
+                print("Found new best model!")
+                confusion_matrix.reset()
+                confusion_matrix.update((y_pred, y_test))
+                top_confusion = confusion_matrix.compute()
+                top_recall = current_recall
+                # top_recall = current_recall
+                # print("Accuracy:", current_acc)
+                # print("Recall:", top_recall)
+                print("Confusion matrix: ", top_confusion)
+                topAcc = current_acc
+                torch.save(model.state_dict, "./models/best_model"+str(topAcc)+".pt")
+
+
         print("Epoch %d: train :̶.̶|̶ ̶:̶;̶ %.4f, test :̶.̶|̶ ̶:̶;̶ %.4f" % (epoch, train_loss, test_loss))
+    print("Accuracy:", topAcc)
+    print("Recall:", top_recall)
+    print("Confusion matrix: ", top_confusion)
 
 
-batch_size = 16
+batch_size = 256
 
-X_train = np.load('processed_data/features_train-v2_upsampled.npy')
-y_train = np.load('processed_data/labels_train-v2_upsampled.npy')
+X_train = np.load('processed_data/features_train-v2_onlyStrest.npy')
+y_train = np.load('processed_data/labels_train-v2_onlyStrest.npy')
 
-X_test = np.load('processed_data/features_test-v2_upsampled.npy')
-y_test = np.load('processed_data/labels_test-v2_upsampled.npy')
+X_test = np.load('processed_data/features_test-v2_onlyStrest.npy')
+y_test = np.load('processed_data/labels_test-v2_onlyStrest.npy')
 
 print(y_test.shape)
 # print(y_test[0,0,:])
